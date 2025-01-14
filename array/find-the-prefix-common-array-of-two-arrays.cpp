ed1a4b6971a3f6a250d57ceb0fb4e955ca68513c
Lu Gan <pursuit.cane@gmail.com>
2025-01-14 19:01:43 +0800

    LC-find-the-prefix-common-array-of-two-arrays [Runtime: 11 ms; Memory: 88.2 MB]

--- array/find-the-prefix-common-array-of-two-arrays.cpp
+++ array/find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+    vector<int> solution1(vector<int>& A, vector<int>& B) {
         unordered_set<int> s;
         vector<int> C(A.size());
         for (int i = 0; i < A.size(); i++) {
@@ -10,6 +10,25 @@ public:
             }
         }
         return C;
+    }
+
+    vector<int> solution2(vector<int>& A, vector<int>& B) {
+        unordered_map<int, int> m;
+        vector<int> C(A.size());
+        for (int i = 0; i < A.size(); i++) {
+            m[A[i]]++;
+            m[B[i]]++;
+            if (i != 0) C[i] = C[i-1];
+            if (m[A[i]] == 2) C[i]++;
+            if (A[i] == B[i]) continue;
+            if (m[B[i]] == 2) C[i]++;
+        }
+        return C;
+    }
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        //return solution1(A, B);
+        return solution2(A, B);
+        
         
     }
 };
