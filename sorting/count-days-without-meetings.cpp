98119a89fd0d964fd6abb387a42c22d4165df18e
Lu Gan <pursuit.cane@gmail.com>
2025-03-24 10:49:06 +0800

    LC-count-days-without-meetings [Runtime: N/A; Memory: N/A]

---
+++ sorting/count-days-without-meetings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int countDays(int days, vector<vector<int>>& meetings) {
+        auto lambda = [](vector<int> a, vector<int> b) {
+            if (a[0] == b[0]) {
+                return a[1] < b[1];
+            }
+            return a[0] < b[0];
+        };
+        sort(meetings.begin(), meetings.end(), lambda);
+        
+        int ans = 0;
+        int curr = 1;
+        for (int i = 0; i < meetings.size(); i++) {
+            int st = meetings[i][0];
+            int end = meetings[i][1];
+            if (st > curr) {
+                ans += (st - curr - 1);
+                curr = end;
+                
+            } else {
+                curr = end;
+            }
+            if (curr >= days) break;
+        }
+        if (curr < days) {
+            ans += days - curr;
+        }
+        return ans;
+
+    }
+};
