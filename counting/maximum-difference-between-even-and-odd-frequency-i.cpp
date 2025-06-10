67ee907df295f10b668ee3b5c4cd6f765b6b2e9c
pursuit <pursuit.cane@gmail.com>
2025-06-10 17:45:27 +0800

    LC-maximum-difference-between-even-and-odd-frequency-i [Runtime: 0 ms; Memory: 9.3 MB]

--- counting/maximum-difference-between-even-and-odd-frequency-i.cpp
+++ counting/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int maxDifference(string s) {
-        
+        vector<int> freq(26);
+        for (auto ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        int max_odd = 0;
+        int min_even = s.size();
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) continue;
+            if (freq[i] & 1) {
+                max_odd = max(max_odd, freq[i]);
+            } else {
+                min_even = min(min_even, freq[i]);
+            }
+        }
+        return max_odd - min_even;
+                
     }
 };
