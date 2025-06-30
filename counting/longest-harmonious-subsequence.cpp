91c4103894faa10a35392d2698e763ddeb891f4a
pursuit <pursuit.cane@gmail.com>
2025-06-30 18:20:30 +0800

    LC-longest-harmonious-subsequence [Runtime: 64 ms; Memory: 45.6 MB]

--- counting/longest-harmonious-subsequence.cpp
+++ counting/longest-harmonious-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int findLHS(vector<int>& nums) {
+    int solution1(vector<int>& nums) {
         set<int> st;
         map<int, int> ht;
         for (auto num : nums) {
@@ -14,6 +14,28 @@ public:
             }
         }
         return ans;
+    }
+    int solution2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        map<int, int> mp;
+        for (auto num : nums) {
+            mp[num]++;
+        }
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i+= mp[nums[i]]) {
+            int num = nums[i];
+            int len = mp[num];
+            if (i + len < nums.size() && nums[i + mp[num]] == num + 1) {
+                ans = max(ans, mp[num] + mp[num + 1]);
+            }
+        }
+        return ans;
+
+    }
+    int findLHS(vector<int>& nums) {
+        //return solution1(nums);
+        return solution2(nums);
+        
         
     }
 };
